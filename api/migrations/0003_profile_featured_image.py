6a21c8c0234fb9d70c29a585693e3d8693aa6826
Gimpneek <colin@gimpneek.com>
2018-05-22 23:02:46 +0100

    - Linting migration

--- api/migrations/0003_profile_featured_image.py
+++ api/migrations/0003_profile_featured_image.py
@@ -14,6 +14,12 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='profile',
             name='featured_image',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.DO_NOTHING, related_name='featured_image', to='api.Image'),
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.DO_NOTHING,
+                related_name='featured_image',
+                to='api.Image'
+            ),
         ),
     ]
