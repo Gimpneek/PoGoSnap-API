bd96fbcbd4782202b218952d02a7faa6579523e6
Gimpneek <colin@gimpneek.com>
2018-11-04 19:50:03 +0000

    - Linting fix

--- api/serializers/collection.py
+++ api/serializers/collection.py
@@ -2,7 +2,6 @@
 """ Serializer for Collection model """
 from rest_framework import serializers
 from api.models.collection import Collection
-from api.serializers.collection_entry import CollectionEntrySerializer
 
 
 class CollectionSerializer(serializers.ModelSerializer):
